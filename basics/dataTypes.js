26a967f2815f990175fb0b94fb896b6ab72f74dd
AKSHA KHAN <aksha.tech@outlook.com>
2026-09-14 09:37:20 +0000

    Complete dataTypes with table representation

--- basics/dataTypes.js
+++ basics/dataTypes.js
@@ -25,3 +25,16 @@ let userCity;
 // symbol => unique
 const id1 = Symbol("123")
 const id2 = Symbol("123")
+
+console.table([
+  typeof score,
+  typeof temperature,
+  typeof largeNum,
+  typeof name,
+  typeof isLoggedIn,
+  typeof isPaymentDone,
+  typeof currTemperature,
+  typeof userCity,
+  typeof id1,
+  typeof id2
+]);
