f6fc6685229f955692f9567b909694c2d013001f
AKSHA KHAN <aksha.tech@outlook.com>
2026-09-14 06:37:44 +0000

    Add test.js in basics

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,10 @@
-console.log("aksha");
+const userId = 1
+let accountEmail = "aksha@gmail.com"
+var accountPassword = "12345"
+accountCity = "Indore"
+let accountState;
+
+// changes in const is not allowed
+// prefer not to use var because of issue in block scope and functional scope
+
+console.table([userId, accountEmail, accountPassword, accountCity, accountState]);
