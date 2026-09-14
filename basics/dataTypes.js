378eaaf1e9b9d83b94106701a3a1a049a07c12ff
AKSHA KHAN <aksha.tech@outlook.com>
2026-09-14 09:34:36 +0000

    Restore dataTypes.js code

---
+++ basics/dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"
+// treat all js code as newer version
+
+// DataTypes =>
+// number
+let score = 95
+let temperature = 34.5
+
+// bigint
+let largeNum = 90073456782376747745n
+
+// string => " "
+let name = "Aksha"
+
+// boolean => true/false
+let isLoggedIn = true
+let isPaymentDone = false
+
+// null => standalone value
+let currTemperature = null
+
+// undefined
+let userCity;
+
+// symbol => unique
+const id1 = Symbol("123")
+const id2 = Symbol("123")
