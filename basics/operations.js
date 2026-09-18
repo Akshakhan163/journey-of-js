bd6f19f420bfffb8c0d80f272bef69e33a18714d
AKSHA KHAN <aksha.tech@outlook.com>
2026-09-18 17:54:14 +0000

    push code in operation.js file

---
+++ basics/operations.js
@@ -0,0 +1,43 @@
+// operations in js
+
+let value = 33
+let negValue = -value
+console.log(negValue)
+// output = -33
+
+console.log(2**3)
+// output = 8 (2^3)
+console.log(2**10)
+// output = 1024 (2^10)
+
+str1 = "Aksha"
+str2 = " Khan"
+
+str3 = str1 + str2;
+console.log(str3)
+// output = Aksha Khan
+
+console.log("1" + 2)
+// output = 12
+console.log(1 + "2")
+// output = 12
+let ans = "1" + 2 + 2
+console.log(ans)
+console.log(typeof ans)
+// output = 122
+// type = string
+let ans1 = 1 + 2 + "2";
+console.log(ans1)
+console.log(typeof ans1);
+// output = 32
+// type = string
+// Note : If we add a string number in any integer number than the type will be string even if we adding two integer numbers with string number and also no matter what's the order.
+
+let num1, num2, num3
+num1 = num2 = num3 = 4
+
+// Increment operators in JS
+let gameCounter = 100
+gameCounter++ 
+++gameCounter
+console.log(gameCounter)
